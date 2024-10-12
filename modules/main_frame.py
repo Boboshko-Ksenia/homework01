6c4e63fec9e1a248e9eafcb7857181bb1bdf6919
Boboshko-Ksenia <you@example.com>
2024-10-12 11:37:12 +0300

    Comments to request_api

--- modules/main_frame.py
+++ modules/main_frame.py
@@ -1,13 +1,14 @@
 import customtkinter
 from .read_json import read_json
 import json
-
+# імпортуємо всі необхідні модулі
 mainframe = customtkinter.CTk(fg_color="#008000")
-#
+# створюємо графічне вікно та задаємо колір тексту
 main_config = read_json(name_file= "config.json")
 # print(json.dumps(main_config, indent= 4))
-#
+# загружаємо файл конфігурації в змінну
 WIDTH = main_config["main_frame"]["width"]
 HEIGHT = main_config["main_frame"]["height"]
-# 
+# задаємо ширину і довжину вікна
 mainframe.geometry(f"{WIDTH}x{HEIGHT}")
+# встановлюємо геометрію вікна у форматі ширина x довготa
--- modules/request_api.py
+++ modules/request_api.py
@@ -1,21 +1,21 @@
 import requests
-#
+# імпортуємо модуль requests для роботи з ним
 from .read_json import read_json
-#
+# з модуля read_json імпортуємо функцію read_json
 import json
-#
+# імпортуємо модуль json
 data_api = read_json(name_file= 'config_api.json')
-#
+# створюємо змінну та задаємо параметр з типом данних str
 API_KEY = data_api['api_key']
-#
+# створюємо константу зі спеціальним ключем для користувача
 CITY_NAME = data_api['city_name']
-#
+# створюємо константу для запису міста
 URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}"
-#
+# константа з адрессою в якій записано місто та ключ
 response = requests.get(URL)
-#
+# відправляємо запрос на отримання адресси
 if response.status_code == 200:
-    #
+    # якщо відповідь була отримана з адресси, то
     data_dict = json.loads(response.content)
-    #
+    # перетворює відповідь з формата json в об'єкт python
     print(json.dumps(data_dict, indent= 4))
